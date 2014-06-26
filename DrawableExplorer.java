df3576ee7383404be08b8b96d7e8b5ce31ae17bd
Arkkraa <akramins@calpoly.edu>
2014-06-25 18:26:45 -0700

    Create DrawableExplorer.java

---
+++ DrawableExplorer.java
@@ -0,0 +1,13 @@
+/**
+ *DrawableExplorer.java
+ *Class DrawableExplorer draws the explorer.
+ *
+ *@author akramins
+ *@version 5/13/2013
+ */
+public class DrawableExplorer extends Explorer implements Drawable{
+	public DrawableExplorer(Square location, Maze maze, String name){
+		super(location, maze, name);
+	}
+	public void draw(){}
+}

d1edc290934dabd0fdfdb551381ed82d5e8a068b
Arkkraa <akramins@calpoly.edu>
2014-06-25 18:27:18 -0700

    Create DrawableMaze.java

---
+++ DrawableMaze.java
@@ -0,0 +1,15 @@
+/**
+ *DrawableMaze.java
+ *Class DrawableMaze draws the maze we will be working with.
+ *
+ *@author akramins
+ *@version 5/13/2013
+ */
+public class DrawableMaze extends Maze implements Drawable{
+	public DrawableMaze(DrawableSquare[][] maze, int rows, int cols){
+		super(maze, rows, cols);
+	}
+	public void draw(){
+		
+	}
+}
